6403796a14f6322384cd09bc522e75922956f848
labustamante_meli <lauravanesa.bustamante@mercadolibre.com.co>
2025-04-02 18:17:13 -0500

    fourth exercise: exceptions
    Division calculation exercise: Exceptions

--- 1. Modulo Java/tre-oop/src/main/java/com/bootcamp/Main.java
+++ 1. Modulo Java/tre-oop/src/main/java/com/bootcamp/Main.java
@@ -1,13 +1,30 @@
 package com.bootcamp;
 
+import com.bootcamp.exceptions.ExceptionsPractice;
 import com.bootcamp.objectsandclasses.Person;
 
 public class Main {
     public static void main(String[] args) {
         executePersonExercise();
+
+        executeExceptionsExercise();
+    }
+
+    private static void executeExceptionsExercise() {
+        System.out.println("\n---Exceptions---\n");
+        ExceptionsPractice ex = new ExceptionsPractice();
+        try {
+            ex.calculateDivision();
+        } catch (ArithmeticException e) {
+            // System.out.println("An error has occurred");
+            throw new IllegalArgumentException("Cannot divide by zero");
+        } finally {
+            System.out.println("Program ended");
+        }
     }
 
     private static void executePersonExercise() {
+        System.out.println("\n---Objects and Classes---\n");
         Person laura = new Person();
         Person lucas = new Person("lucas", 23, "12456321");
         Person carolina = new Person("carolina", 24, "12456321", 60, 1.62);
---
+++ 1. Modulo Java/tre-oop/src/main/java/com/bootcamp/exceptions/ExceptionsPractice.java
@@ -0,0 +1,10 @@
+package com.bootcamp.exceptions;
+
+public class ExceptionsPractice {
+    Integer a = 0;
+    Integer b = 300;
+
+    public Integer calculateDivision() {
+        return b / a;
+    }
+}
