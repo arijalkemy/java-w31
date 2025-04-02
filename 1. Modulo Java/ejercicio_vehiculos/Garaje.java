1d97aa1ccd481082bd689e1936b85d8deafd415e
Mateo Olaya <mateoolaya20032604@gmail.com>
2025-04-02 09:40:59 -0500

    add ejercicio vehiculos

---
+++ 1. Modulo Java/ejercicio_vehiculos/Garaje.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.OptionalDouble;
+
+public class Garaje {
+    private Integer id;
+    private ArrayList<Vehiculo> vehiculos;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ArrayList<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(ArrayList<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public Garaje(Integer id) {
+        this.id = id;
+        this.vehiculos = new ArrayList<Vehiculo>();
+    }
+
+    public void agregarVehiculo(Vehiculo vehiculo) {
+        this.vehiculos.add(vehiculo);
+    }
+
+    public void ordenarMenorAMayorPrecio() {
+        vehiculos.stream().sorted((x, y) -> x.getCosto().compareTo(y.getCosto())).forEach(System.out::println);
+    }
+
+    public void ordenarMarcaYPrecio() {
+        vehiculos.stream().sorted((x, y) -> {
+            int marcaComparison = x.getMarca().compareTo(y.getMarca());
+            if (marcaComparison != 0) {
+                return marcaComparison;
+            } else {
+                return x.getCosto().compareTo(y.getCosto());
+            }
+        }).forEach(System.out::println);
+    }
+
+    public void precioMenorMil() {
+        vehiculos.stream().filter(vehiculo -> vehiculo.getCosto() < 1000).forEach(System.out::println);
+    }
+
+    public void precioMayorIgualMil() {
+        vehiculos.stream().filter(vehiculo -> vehiculo.getCosto() >= 1000).forEach(System.out::println);
+    }
+
+    public Double promedioPrecios() {
+        return vehiculos.stream().mapToDouble(vehiculo -> vehiculo.getCosto()).average().getAsDouble();
+    }
+
+}
---
+++ 1. Modulo Java/ejercicio_vehiculos/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        Garaje garage = new Garaje(1);
+        garage.agregarVehiculo(new Vehiculo("Ford", "Fiesta", 1000.0));
+        garage.agregarVehiculo(new Vehiculo("Ford", "Focus", 1200.0));
+        garage.agregarVehiculo(new Vehiculo("Ford", "Explorer", 2500.0));
+        garage.agregarVehiculo(new Vehiculo("Fiat", "Uno", 500.0));
+        garage.agregarVehiculo(new Vehiculo("Fiat", "Cronos", 1000.0));
+        garage.agregarVehiculo(new Vehiculo("Fiat", "Torino", 1250.0));
+        garage.agregarVehiculo(new Vehiculo("Chevrolet", "Aveo", 1250.0));
+        garage.agregarVehiculo(new Vehiculo("Chevrolet", "Spin", 2500.0));
+        garage.agregarVehiculo(new Vehiculo("Toyota", "Corolla", 1200.0));
+        garage.agregarVehiculo(new Vehiculo("Toyota", "Fortuner", 3000.0));
+        garage.agregarVehiculo(new Vehiculo("Renault", "Logan", 950.0));
+
+        System.out.println("Lista de vehiculos ordenados por precio de menor a mayor:");
+        garage.ordenarMenorAMayorPrecio();
+        System.out.println("\nLista de vehiculos ordenados por marca y precio:");
+        garage.ordenarMarcaYPrecio();
+
+        System.out.println("\nLista de vehiculos con precio menor a 100:");
+        garage.precioMenorMil();
+
+        System.out.println("\nLista de vehiculos con precio mayor o igual a 1000:");
+        garage.precioMayorIgualMil();
+
+        System.out.println("Promedio de precios de los vehiculos:");
+        System.out.println(garage.promedioPrecios());
+    }
+}
