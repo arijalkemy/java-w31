ff52f60741ffec822ea82e516761b2ab92ccd0f1
femartinez <femartinez@fi.uba.ar>
2025-04-03 09:20:24 -0300

    Corrigiendo un var que quedo suelto

--- 1. Modulo Java/src/main/java/ejercicioGenerics/Main.java
+++ 1. Modulo Java/src/main/java/ejercicioGenerics/Main.java
@@ -3,6 +3,7 @@ package ejercicioGenerics;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -39,11 +40,18 @@ public class Main {
         List<Vehiculo> vehiclesMoreThan1000 = garaje.getVehiculos().stream().filter(vehiculo -> vehiculo.getCosto() >= 1000).toList();
         vehiclesMoreThan1000.forEach(System.out::println);
 
-        var particionados = garaje.getVehiculos().stream().collect(Collectors.partitioningBy(v -> v.getCosto() > 1000));
-
         double averagePrice = garaje.getVehiculos().stream().map(Vehiculo::getCosto).collect(Collectors.averagingInt(v -> v));
+        double totalPrice = garaje.getVehiculos().stream().map(Vehiculo::getCosto).reduce(0, Integer::sum);
+        double avgPrice = totalPrice / garaje.getVehiculos().size();
+        System.out.println("Average price: " + avgPrice);
         System.out.println("Precio promedio de los vehiculos: " + averagePrice);
 
+        Map<Boolean, List<Vehiculo>> particionados = garaje.getVehiculos().stream().collect(Collectors.partitioningBy(v -> v.getCosto() >= 1000));
+        List<Vehiculo> mayores = particionados.get(true);
+        List<Vehiculo> menores = particionados.get(false);
+
+        System.out.println("\n Con particionadom mayores a 1000:\n" + mayores);
+        System.out.println("\n Con particionado menores a 1000:\n" + menores);
     }
 
 }
