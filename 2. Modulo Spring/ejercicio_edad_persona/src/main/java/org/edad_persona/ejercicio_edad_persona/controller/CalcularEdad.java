367be85b9967dcf21d2e14e77a6ed63e90328dcf
JuanIgnacioBellavitis <juan.bellavitis@gmail.com>
2025-04-04 16:30:44 -0300

    Agrego validaciones y RequestMapping

--- 2. Modulo Spring/ejercicio_edad_persona/src/main/java/org/edad_persona/ejercicio_edad_persona/controller/CalcularEdad.java
+++ 2. Modulo Spring/ejercicio_edad_persona/src/main/java/org/edad_persona/ejercicio_edad_persona/controller/CalcularEdad.java
@@ -2,22 +2,28 @@ package org.edad_persona.ejercicio_edad_persona.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.Period;
 
 @RestController
+@RequestMapping("/edad")
 public class CalcularEdad {
 
     @GetMapping("{dia}/{mes}/{anio}")
-    public String CalcularEdad(@PathVariable String dia, @PathVariable String mes, @PathVariable String anio) {
+    public String calcularEdad(@PathVariable String dia, @PathVariable String mes, @PathVariable String anio) {
+        try {
+            LocalDate fechaNacimiento = LocalDate.of(Integer.parseInt(anio), Integer.parseInt(mes), Integer.parseInt(dia));
+            LocalDate fechaActual = LocalDate.now();
 
-        LocalDate fechaNacimiento = LocalDate.of(Integer.parseInt(anio), Integer.parseInt(mes), Integer.parseInt(dia));
-        LocalDate fechaActual = LocalDate.now();
+            Period edad = Period.between(fechaNacimiento, fechaActual);
 
-        Period edad = Period.between(fechaNacimiento, fechaActual);
-
-        return "El usuario tiene " + edad.getYears() + " años";
+            return "El usuario tiene " + edad.getYears() + " años";
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
     }
+
 }
