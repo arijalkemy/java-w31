70c3dc351c20f7c5d50e7cce9978b9cb6266c23b
JuanIgnacioBellavitis <juan.bellavitis@gmail.com>
2025-04-04 11:43:16 -0300

    numeros normales a romanos

---
+++ 2. Modulo Spring/ejercicio_numerosRomandos/src/main/java/org/numeros_romanos/ejercicio_numerosromandos/EjercicioNumerosRomandosApplication.java
@@ -0,0 +1,13 @@
+package org.numeros_romanos.ejercicio_numerosromandos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioNumerosRomandosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioNumerosRomandosApplication.class, args);
+    }
+
+}
---
+++ 2. Modulo Spring/ejercicio_numerosRomandos/src/test/java/org/numeros_romanos/ejercicio_numerosromandos/EjercicioNumerosRomandosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.numeros_romanos.ejercicio_numerosromandos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioNumerosRomandosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
