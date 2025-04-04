0f9dc12fb361bf77b54e9ffcce0c3f0856b5e990
frankfelipro_meli <frankfelipe.rojas@mercadolibre.com.co>
2025-04-04 11:47:01 -0500

    Ejercicio romanos

---
+++ 2. Modulo Spring/ejercicio_romanos/src/main/java/com/example/ejercicio_romanos/EjercicioRomanosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio_romanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioRomanosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioRomanosApplication.class, args);
+    }
+
+}
---
+++ 2. Modulo Spring/ejercicio_romanos/src/test/java/com/example/ejercicio_romanos/EjercicioRomanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio_romanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioRomanosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
