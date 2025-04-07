4b11ba499cde33106b1458356bbd7bbb2df337a1
David Alexander Varon Duarte <david.varon@mercadolibre.com.co>
2025-04-07 07:04:56 -0500

    Introduccion a java Spring lista

---
+++ 1. Modulo Java/2. Ejercicio introduccion 2/src/main/java/com/mercadolibre/codigomorse/controller/MessagesController.java
@@ -0,0 +1,20 @@
+package com.mercadolibre.codigomorse.controller;
+
+
+import com.mercadolibre.codigomorse.repository.ConvertMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MessagesController {
+    private final ConvertMessage message;
+
+    @GetMapping("/ObtenerPalabra")
+    public String getWord(@RequestParam String word) {
+        return message.convertToMessage(word);
+    }
+
+}
---
+++ 1. Modulo Java/2. Ejercicio introduccion 2/src/main/java/com/mercadolibre/codigomorse/model/Garment.java
@@ -0,0 +1,16 @@
+package com.mercadolibre.codigomorse.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Garment {
+    private int id;
+    private String model;
+    private String brand;
+}
