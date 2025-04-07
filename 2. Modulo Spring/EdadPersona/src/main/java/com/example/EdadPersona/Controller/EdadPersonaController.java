32eb1f7f1da23d11d3f4396837659b612082fea4
bercellinierika@gmail.com <erika.bercellini@mercadolibre.com>
2025-04-07 12:06:14 -0300

    Agrego ejercicios modulo Spring

---
+++ 2. Modulo Spring/EdadPersona/src/main/java/com/example/EdadPersona/Controller/EdadPersonaController.java
@@ -0,0 +1,33 @@
+package com.example.EdadPersona.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.example.EdadPersona.Service.EdadPersonaService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/*
+ * Se necesita desarrollar un API que recibe como parámetro tres valores:
+ *  - Día
+ *  - Mes
+ *  - Año
+ * 
+ * Dichos valores corresponden a la fecha de nacimiento de una persona y deben ser valores
+ * enteros. Por ejemplo, un ingreso válido sería: 10/01/1990.
+ * Para este caso, es necesario que la fecha de nacimiento se pase mediante la URL del
+ * navegador. Por ejemplo: http://localhost:8080/10/01/1990. Como resultado, la API deberá
+ * devolver la edad de la persona.
+ * En el navegador se debería ver algo similar:
+ */
+@RestController
+public class EdadPersonaController {
+    @Autowired
+    EdadPersonaService edadPersonaService;
+
+    @GetMapping("/{dia}/{mes}/{anio}")
+    public String getEdadPersona(@PathVariable Integer dia, @PathVariable Integer mes, @PathVariable Integer anio) {
+        return edadPersonaService.calcularEdad(dia, mes, anio);
+    }
+    
+}
---
+++ 2. Modulo Spring/EdadPersona/src/main/java/com/example/EdadPersona/Service/EdadPersonaService.java
@@ -0,0 +1,28 @@
+package com.example.EdadPersona.Service;
+
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+
+@Service
+public class EdadPersonaService {
+    public String calcularEdad(Integer dia, Integer mes, Integer anio) {
+        if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || anio < 1900) {
+            return "Fecha inválida.";
+        }
+
+        LocalDate hoy = LocalDate.now();
+        if ((anio == hoy.getYear() && mes == hoy.getMonthValue() && dia >= hoy.getDayOfMonth()) 
+            || (anio == hoy.getYear() && mes >= hoy.getMonthValue()) 
+            || (anio > hoy.getYear())) {
+            return "No nació todavía.";
+        }
+
+        LocalDate nacimiento = LocalDate.of(anio, mes, dia);
+
+        Integer edad = hoy.getYear() - nacimiento.getYear();
+        if (hoy.getDayOfYear() < nacimiento.getDayOfYear()) {
+            edad--;
+        }
+        return Integer.toString(edad);
+    }
+}
