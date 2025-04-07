c31f5a6f476bd5f2d63940a216b97e707958f56c
Miguel Alfredo Bustos <miguel.bustos@mercadolibre.com>
2025-04-07 12:34:17 -0300

    Ejercicio modificado con clases LocalDate

--- 2. Modulo Spring/edaddeunapersona/src/main/java/com/edaddeunapersona/edaddeunapersona/controller/EdadDeUnaPersona.java
+++ 2. Modulo Spring/edaddeunapersona/src/main/java/com/edaddeunapersona/edaddeunapersona/controller/EdadDeUnaPersona.java
@@ -1,33 +1,42 @@
 package com.edaddeunapersona.edaddeunapersona.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Map;
 
 @RestController
 public class EdadDeUnaPersona {
 
     @GetMapping(path = "{dia}/{mes}/{anio}")
-    public Integer consultarEdad(@PathVariable Integer dia,
-                                 @PathVariable Integer mes,
-                                 @PathVariable Integer anio
+    public ResponseEntity<?> consultarEdad(@PathVariable Integer dia,
+                                                 @PathVariable Integer mes,
+                                                 @PathVariable Integer anio
     ){
-        //Supongo que se valida que los datos ingresados esten un rango valido
-        //Fecha hoy
-        Integer diaActual = 7,mesActual = 4,anioActual = 2025, resultado = 0;
+        //Fecha actual
+        LocalDate fechaHoy = LocalDate.now();
 
-        //Calculo por año
-        resultado = anioActual - anio;
+        //Fecha recibida
+        LocalDate fechaRecibida = LocalDate.of(anio,mes,dia);
 
-        //Chequeo por mes
-        if(mesActual <mes){
-            resultado --;
-        }else if(mesActual.equals(mes)){
-            //Chequedo día
-            if(diaActual<dia){
-                resultado--;
-            }
+        //Chequeo que la fecha no sea futura
+        if (fechaRecibida.isAfter(fechaHoy)) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(Map.of("Error", "La fecha de nacimiento no puede ser en el futuro"));
         }
-        return resultado;
+
+        //Cálcula la diferencia
+        Period edad = Period.between(fechaRecibida,fechaHoy);
+
+        return ResponseEntity.ok(edad.getYears());
     }
+
 }
