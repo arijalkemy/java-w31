be3b09c0d3cdfc98742dc970a7750ccd4da77592
JuanIgnacioBellavitis <juan.bellavitis@gmail.com>
2025-04-08 11:25:36 -0300

    Exceptions Controller

---
+++ 2. Modulo Spring/ejercicio_starWars/src/main/java/org/mercadolibre/ejercicio_starwars/dto/ExceptionDTO.java
@@ -0,0 +1,13 @@
+package org.mercadolibre.ejercicio_starwars.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ExceptionDTO {
+    private String message;
+
+}
