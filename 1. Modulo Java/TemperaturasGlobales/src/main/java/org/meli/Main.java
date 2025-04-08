e3269eef4a1258e8a5cc1bcd5aef53bf4a121b0d
angarellano_meli <angelarmando.arellano@mercadolibre.com.mx>
2025-04-07 19:26:01 -0600

    add: Global Temperatures practice exercise

---
+++ 1. Modulo Java/TemperaturasGlobales/src/main/java/org/meli/Main.java
@@ -0,0 +1,35 @@
+package org.meli;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] ciudades = {
+                "Londres", "Madrid", "Nueva York", "Buenos Aires", "Asunción",
+                "São Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"
+        };
+
+        int[][] temperaturas = {
+                {-2, 33}, {-3, 32}, {-8, 27}, {4, 37}, {6, 42},
+                {5, 43}, {0, 39}, {-7, 26}, {-1, 31}, {-10, 35}
+        };
+
+        int tempMin = temperaturas[0][0];
+        int tempMax = temperaturas[0][1];
+        String ciudadMin = ciudades[0];
+        String ciudadMax = ciudades[0];
+
+        for (int i = 0; i < temperaturas.length; i++) {
+            if (temperaturas[i][0] < tempMin) {
+                tempMin = temperaturas[i][0];
+                ciudadMin = ciudades[i];
+            }
+            if (temperaturas[i][1] > tempMax) {
+                tempMax = temperaturas[i][1];
+                ciudadMax = ciudades[i];
+            }
+        }
+
+        System.out.println("La menor temperatura la tuvo " + ciudadMin + ", con " + tempMin + "º C.");
+        System.out.println("La mayor temperatura la tuvo " + ciudadMax + ", con " + tempMax + "º C.");
+
+    }
+}
