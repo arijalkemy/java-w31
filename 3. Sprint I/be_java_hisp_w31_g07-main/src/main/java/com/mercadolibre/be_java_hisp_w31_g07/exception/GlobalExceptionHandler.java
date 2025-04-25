8937a01218d7397c5560da25ba6f240afc85c22f
thomasbarenghi <thomasbarenghi@gmail.com>
2025-04-25 10:31:44 -0300

    upload sprint 1

---
+++ 3. Sprint I/be_java_hisp_w31_g07-main/src/main/java/com/mercadolibre/be_java_hisp_w31_g07/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.mercadolibre.be_java_hisp_w31_g07.exception;
+
+import com.mercadolibre.be_java_hisp_w31_g07.dto.GlobalExceptioHandlerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BadRequest.class)
+    public ResponseEntity<?> badRequest(BadRequest e) {
+        GlobalExceptioHandlerDto exceptionDto = new GlobalExceptioHandlerDto(e.getMessage());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> typeMismatch(MethodArgumentTypeMismatchException e) {
+        String param = e.getName();
+        String message = "Parameter '" + param + "' is invalid.";
+        GlobalExceptioHandlerDto dto = new GlobalExceptioHandlerDto(message);
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ 3. Sprint I/be_java_hisp_w31_g07-main/src/main/java/com/mercadolibre/be_java_hisp_w31_g07/repository/implementations/SellerRepository.java
@@ -0,0 +1,71 @@
+package com.mercadolibre.be_java_hisp_w31_g07.repository.implementations;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.be_java_hisp_w31_g07.model.Buyer;
+import com.mercadolibre.be_java_hisp_w31_g07.model.Seller;
+import com.mercadolibre.be_java_hisp_w31_g07.repository.ISellerRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class SellerRepository implements ISellerRepository {
+    private List<Seller> sellerList = new ArrayList<>();
+
+    public SellerRepository() throws IOException {
+        loadDataBaseSeller();
+    }
+
+    private void loadDataBaseSeller() throws IOException {
+        File file;
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Seller> sellers;
+
+        file = ResourceUtils.getFile("classpath:seller.json");
+        sellers = objectMapper.readValue(file, new TypeReference<List<Seller>>() {
+        });
+
+        sellerList = sellers;
+    }
+
+    @Override
+    public Optional<Seller> addBuyerToFollowersList(Buyer buyer, UUID sellerId) {
+        return this.findSellerById(sellerId)
+                .map(seller -> {
+                    seller.addFollower(buyer);
+                    seller.incrementFollowerCount();
+                    return seller;
+                });
+    }
+
+    @Override
+    public Optional<Seller> findSellerById(UUID userId) {
+        return sellerList.stream()
+                .filter(seller -> seller.getId().equals(userId))
+                .findFirst();
+    }
+
+    @Override
+    public boolean sellerIsBeingFollowedByBuyer(Buyer buyer, UUID sellerId) {
+        return sellerList.stream()
+                .filter(seller -> seller.getId().equals(sellerId))
+                .flatMap(seller -> seller.getFollowers().stream())
+                .anyMatch(follower -> follower.getId().equals(buyer.getId()));
+    }
+
+    @Override
+    public void removeBuyerFromFollowersList(Buyer buyer, UUID sellerId) {
+        this.findSellerById(sellerId).map(seller -> {
+            seller.removeFollower(buyer);
+            seller.decrementFollowerCount();
+            return seller;
+        });
+    }
+}
