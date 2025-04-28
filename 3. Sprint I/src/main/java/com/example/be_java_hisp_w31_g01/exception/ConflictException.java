d293c39a73f96ffcd8771aff0c00823bee1f0211
SpiconaAnaliaMELI <analia.delvalle+public@mercadolibre.com>
2025-04-28 11:20:54 -0300

    proyecto SocialMeli sprint 1

--- 3. Sprint I/src/main/java/com/example/be_java_hisp_w31_g01/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.example.be_java_hisp_w31_g01.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
