619ebc806448a4c96dfe92895664e5e481ed5379
JuanIgnacioBellavitis <juan.bellavitis@gmail.com>
2025-04-28 15:37:26 -0300

    Tests with +80% of coverage

---
+++ 4. Testing/calculadora-metros-cuadrados/src/main/java/com/mercadolibre/calculadorametroscuadrados/dto/HouseResponseDTO.java
@@ -0,0 +1,25 @@
+package com.mercadolibre.calculadorametroscuadrados.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Setter
+@Getter
+@EqualsAndHashCode(callSuper = true)
+public class HouseResponseDTO extends HouseDTO {
+  private Integer squareFeet;
+  private Integer price;
+  private RoomDTO biggest;
+
+  public HouseResponseDTO() {
+  }
+
+  public HouseResponseDTO(HouseDTO house) {
+    this.setName(house.getName());
+    this.setAddress(house.getAddress());
+    this.setRooms(house.getRooms());
+  }
+}
