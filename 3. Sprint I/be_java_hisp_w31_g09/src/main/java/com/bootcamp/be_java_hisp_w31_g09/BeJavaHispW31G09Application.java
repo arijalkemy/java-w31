aeddab6949193c88bcdc8cf5a8ffaa267f99349e
LucasV7-T <lucas.vilas@mercadolibre.com>
2025-04-29 17:05:42 -0300

    sprint 1

---
+++ 3. Sprint I/be_java_hisp_w31_g09/src/main/java/com/bootcamp/be_java_hisp_w31_g09/BeJavaHispW31G09Application.java
@@ -0,0 +1,13 @@
+package com.bootcamp.be_java_hisp_w31_g09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaHispW31G09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeJavaHispW31G09Application.class, args);
+	}
+
+}
