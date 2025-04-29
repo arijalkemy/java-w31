64d7c314ebf83820b14778b15e047b73c675b49f
dmojica1958 <diego.mojica@mercadolibre.com.co>
2025-04-29 10:47:42 -0500

    add the practice exercise in live coding

---
+++ 4. Testing/calculadora-metros-cuadrados/src/test/java/com/mercadolibre/calculadorametroscuadrados/controller/ControllerTests.java
@@ -0,0 +1,53 @@
+package com.mercadolibre.calculadorametroscuadrados.controller;
+
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseResponseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.RoomDTO;
+import com.mercadolibre.calculadorametroscuadrados.service.CalculateService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class ControllerTests {
+
+    @Mock
+    private CalculateService calculateService;
+
+    @InjectMocks
+    private CalculateRestController controller;
+
+    @Test
+    public void testCalculate() {
+        // Arrange
+        List<RoomDTO> roomDTOList = new ArrayList<>();
+        RoomDTO room1 = new RoomDTO("Hab 1", 5, 10);
+        RoomDTO room2 = new RoomDTO("Hab 2", 9, 20);
+        roomDTOList.add(room1);
+        roomDTOList.add(room2);
+
+        HouseDTO house = new HouseDTO("Casa 1", "Address", roomDTOList);
+
+        HouseResponseDTO response = new HouseResponseDTO(house);
+        response.setSquareFeet(230);
+        response.setBiggest(room2);
+
+        // Act
+        Mockito.when(calculateService.calculate(house)).thenReturn(response);
+        HouseResponseDTO houseResponse = controller.calculate(house);
+
+        // Assert
+        Mockito.verify(calculateService, Mockito.atLeast(1)).calculate(house);
+        Assertions.assertEquals(response, houseResponse);
+    }
+}
---
+++ 4. Testing/calculadora-metros-cuadrados/src/test/java/com/mercadolibre/calculadorametroscuadrados/integration/ControllerTests.java
@@ -0,0 +1,60 @@
+package com.mercadolibre.calculadorametroscuadrados.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseResponseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.RoomDTO;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testCalculate() throws Exception{
+        List<RoomDTO> roomDTOList = new ArrayList<>();
+        RoomDTO room1 = new RoomDTO("Hab 1", 5, 10);
+        RoomDTO room2 = new RoomDTO("Hab 2", 9, 20);
+        roomDTOList.add(room1);
+        roomDTOList.add(room2);
+
+        HouseDTO house = new HouseDTO("Casa 1", "Address", roomDTOList);
+
+        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer();
+
+        String payloadJson = writer.writeValueAsString(house);
+
+        HouseResponseDTO response = new HouseResponseDTO(house);
+        response.setSquareFeet(230);
+        response.setBiggest(room2);
+
+        // Act & Assert
+        this.mockMvc.perform(post("/calculate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payloadJson))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value(response.getName()))
+                .andExpect(jsonPath("$.biggest.squareFeet").value(response.getBiggest().getSquareFeet()));
+    }
+}
