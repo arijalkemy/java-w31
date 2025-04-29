5beac27208033f60da82c1d6a6a429a46beb64c1
Juan Martin Caparros <juancaparr0s@hotmail.com>
2025-04-29 12:07:00 -0300

    Add testing exercise m2

---
+++ 4. Testing/calculadora-metros-cuadrados/src/test/java/com/mercadolibre/calculadorametroscuadrados/service/CalculateServiceTest.java
@@ -0,0 +1,76 @@
+package com.mercadolibre.calculadorametroscuadrados.service;
+
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseResponseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.RoomDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest(classes = CalculateService.class)
+public class CalculateServiceTest {
+
+    @Autowired
+    private CalculateService calculateService;
+
+    @Test
+    public void calculateHousePriceTest() {
+        //ARRANGE
+        Integer price = 100;
+        HouseDTO houseDTO = new HouseDTO("Polo Dot", "Posta xxxx", List.of(
+                new RoomDTO("Room 1", 10, 20),
+                new RoomDTO("Room 2", 20, 30)
+        ));
+
+        //ACT
+        HouseResponseDTO houseResponseDTO = calculateService.calculate(houseDTO);
+
+        //ASSERT
+        Assertions.assertEquals(houseResponseDTO.getPrice(), (10*20 + 20*30)*800);
+    }
+
+    @Test
+    public void calculateBiggestRoom() {
+        //ARRANGE
+        Integer price = 100;
+
+        RoomDTO room1 = new RoomDTO("Room 1", 10, 20);
+        RoomDTO room2 = new RoomDTO("Room 2", 20, 30);
+        HouseDTO houseDTO = new HouseDTO("Polo Dot", "Posta xxxx", List.of(
+                room1,
+                room2
+        ));
+
+        //ACT
+        HouseResponseDTO houseResponseDTO = calculateService.calculate(houseDTO);
+
+        //ASSERT
+        Assertions.assertEquals(houseResponseDTO.getBiggest(), room2);
+    }
+
+    @Test
+    public void calculateSquareFeetPerRoom() {
+        //ARRANGE
+        Integer price = 100;
+
+        RoomDTO room1 = new RoomDTO("Room 1", 10, 20);
+        RoomDTO room2 = new RoomDTO("Room 2", 20, 30);
+        HouseDTO houseDTO = new HouseDTO("Polo Dot", "Posta xxxx", List.of(
+                room1,
+                room2
+        ));
+
+        //ACT
+        HouseResponseDTO houseResponseDTO = calculateService.calculate(houseDTO);
+
+        //ASSERT
+        Assertions.assertEquals(houseResponseDTO.getRooms().get(0).getSquareFeet(), room1.getLength()*room1.getWidth());
+        Assertions.assertEquals(houseResponseDTO.getRooms().get(1).getSquareFeet(), room2.getLength()*room2.getWidth());
+    }
+
+}
