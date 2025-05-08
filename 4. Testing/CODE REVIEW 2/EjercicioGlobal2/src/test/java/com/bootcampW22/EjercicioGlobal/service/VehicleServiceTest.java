fc2dce4a36aeb35208f87e6226cc7126a436272a
CarolinaSeoane <carolina.b.seoane@gmail.com>
2025-05-08 09:44:41 -0300

    CR2 improvements to VehicleService unit tests

--- 4. Testing/CODE REVIEW 2/EjercicioGlobal2/src/test/java/com/bootcampW22/EjercicioGlobal/service/VehicleServiceTest.java
+++ 4. Testing/CODE REVIEW 2/EjercicioGlobal2/src/test/java/com/bootcampW22/EjercicioGlobal/service/VehicleServiceTest.java
@@ -7,7 +7,6 @@ import com.bootcampW22.EjercicioGlobal.entity.Vehicle;
 import com.bootcampW22.EjercicioGlobal.exception.NotFoundException;
 import com.bootcampW22.EjercicioGlobal.repository.VehicleRepositoryImpl;
 import com.bootcampW22.EjercicioGlobal.utils.VehicleFactory;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,10 +18,10 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class VehicleServiceTest {
+class VehicleServiceTest {
 
     @Mock
     private VehicleRepositoryImpl vehicleRepository;
@@ -34,47 +33,70 @@ public class VehicleServiceTest {
 
     @Test
     @DisplayName("[SUCCESS] Unit test: Get vehicles by year and color")
-    void searchVehiclesByYearAndColor() {
+    void searchVehiclesByColorYearAndColor_ShouldReturnList_WhenColorAndYearMatch() {
+        String color = "green";
+        int year = 2005;
+
         // Arrange
         List<Vehicle> vehicleList = List.of(VehicleFactory.vehicle1);
         List<VehicleDto> expectedResult = List.of(VehicleFactory.convertToVehicleDto(VehicleFactory.vehicle1));
-        when(vehicleRepository.findVehiclesByYearAndColor("green", 2005)).thenReturn(vehicleList);
+        when(vehicleRepository.findVehiclesByYearAndColor(color, year)).thenReturn(vehicleList);
 
         // Act
-        List<VehicleDto> result = vehicleService.searchVehiclesByYearAndColor("green", 2005);
+        List<VehicleDto> result = vehicleService.searchVehiclesByYearAndColor(color, year);
 
         // Assert
         assertAll(
                 () -> assertEquals(1, result.size()),
                 () -> assertEquals(expectedResult.getFirst(), result.getFirst())
         );
+
+        verify(vehicleRepository).findVehiclesByYearAndColor(color, year);
+        verifyNoMoreInteractions(vehicleRepository);
     }
 
     @Test
     @DisplayName("[ERROR 404] Unit test: Get vehicles by year and color - Not found")
-    void searchVehiclesByYearAndColorNotFound() {
+    void searchVehiclesByColorYearAndColor_ShouldReturnError_WhenColorAndYearDontMatch() {
+        String color = "blue";
+        int year = 2019;
+
         // Arrange
-        when(vehicleRepository.findVehiclesByYearAndColor("blue", 2019))
+        when(vehicleRepository.findVehiclesByYearAndColor(color, year))
                 .thenReturn(Collections.emptyList());
 
-        // Act & Assert
-        assertThrows(NotFoundException.class, () ->
-                vehicleService.searchVehiclesByYearAndColor("blue", 2019)
+        // Act
+        NotFoundException notFoundException = assertThrows(NotFoundException.class, () ->
+                vehicleService.searchVehiclesByYearAndColor(color, year)
         );
+
+        // Assert
+        assertAll(
+                ()-> assertEquals("No se encontraron vehículos con esos criterios."
+                        ,notFoundException.getMessage()),
+                ()-> assertNotNull(notFoundException)
+        );
+
+        verify(vehicleRepository).findVehiclesByYearAndColor(color, year);
+        verifyNoMoreInteractions(vehicleRepository);
     }
 
     /* Punto 2 */
 
     @Test
     @DisplayName("[SUCCESS] Unit test: Get vehicles by brand and range of year")
-    void searchVehiclesByBrandAndRangeOfYear() {
+    void searchVehiclesByBrandAndRangeOfYear_ShouldReturnList_WhenBrandAndYearsMatch() {
+        String brand = "toyota";
+        int startYear = 2000;
+        int endYear = 2006;
+
         // Arrange
         List<Vehicle> vehicleList = List.of(VehicleFactory.vehicle3);
         List<VehicleDto> expectedResult = List.of(VehicleFactory.convertToVehicleDto(VehicleFactory.vehicle3));
-        when(vehicleRepository.findVehiclesByBrandAndRangeOfYear("toyota", 2000, 2006)).thenReturn(vehicleList);
+        when(vehicleRepository.findVehiclesByBrandAndRangeOfYear(brand, startYear, endYear)).thenReturn(vehicleList);
 
         // Act
-        List<VehicleDto> result = vehicleService.searchVehiclesByBrandAndRangeOfYear("toyota", 2000, 2006);
+        List<VehicleDto> result = vehicleService.searchVehiclesByBrandAndRangeOfYear(brand, startYear, endYear);
 
         // Assert
         assertAll(
@@ -85,14 +107,25 @@ public class VehicleServiceTest {
 
     @Test
     @DisplayName("[ERROR 404] Unit test: Get vehicles by brand and range of year - Not found")
-    void searchVehiclesByBrandAndRangeOfYearNotFound() {
+    void searchVehiclesByBrandAndRangeOfYear_ShouldReturnError_WhenBrandAndYearsDontMatch() {
+        String brand = "bmw";
+        int startYear = 2015;
+        int endYear = 2019;
+
         // Arrange
-        when(vehicleRepository.findVehiclesByBrandAndRangeOfYear("bmw", 2015, 2019))
+        when(vehicleRepository.findVehiclesByBrandAndRangeOfYear(brand, startYear, endYear))
                 .thenReturn(Collections.emptyList());
 
-        // Act & Assert
-        assertThrows(NotFoundException.class, () ->
-                vehicleService.searchVehiclesByBrandAndRangeOfYear("bmw", 2015, 2019)
+        // Act
+        NotFoundException notFoundException = assertThrows(NotFoundException.class, () ->
+                vehicleService.searchVehiclesByBrandAndRangeOfYear(brand, startYear, endYear)
+        );
+
+        // Assert
+        assertAll(
+                ()-> assertEquals("No se encontraron vehículos con esos criterios."
+                        ,notFoundException.getMessage()),
+                ()-> assertNotNull(notFoundException)
         );
     }
 
@@ -100,17 +133,20 @@ public class VehicleServiceTest {
 
     @Test
     @DisplayName("[SUCCESS] Unit test: Get average speed by brand")
-    void calculateAvgSpeedByBrand() {
+    void calculateAvgSpeedByBrand_ShouldReturnAvgSpeed_WhenBrandHasVehicles() {
+        String brand = "lexus";
+        double expectedAvgSpeed = 134.5D;
+
         // Arrange
         Vehicle vehicle2 = VehicleFactory.vehicle2;
         Vehicle vehicle4 = VehicleFactory.vehicle4;
         List<Vehicle> vehicleList = List.of(vehicle2, vehicle4);
-        VehicleAvgSpeedByBrandDto expected = new VehicleAvgSpeedByBrandDto(134.5D);
+        VehicleAvgSpeedByBrandDto expected = new VehicleAvgSpeedByBrandDto(expectedAvgSpeed);
 
-        when(vehicleRepository.findVehiclesByBrand("lexus")).thenReturn(vehicleList);
+        when(vehicleRepository.findVehiclesByBrand(brand)).thenReturn(vehicleList);
 
         // Act
-        VehicleAvgSpeedByBrandDto result = vehicleService.calculateAvgSpeedByBrand("lexus");
+        VehicleAvgSpeedByBrandDto result = vehicleService.calculateAvgSpeedByBrand(brand);
 
         // Assert
         assertEquals(expected, result);
@@ -118,14 +154,23 @@ public class VehicleServiceTest {
 
     @Test
     @DisplayName("[ERROR 404] Unit test: Get average speed by brand - Not found")
-    void calculateAvgSpeedByBrandNotFound() {
+    void calculateAvgSpeedByBrand_ShouldReturnError_WhenBrandHasNoVehicles() {
+        String brand = "bmw";
+
         // Arrange
-        when(vehicleRepository.findVehiclesByBrand("bmw"))
+        when(vehicleRepository.findVehiclesByBrand(brand))
                 .thenReturn(Collections.emptyList());
 
-        // Act & Assert
-        assertThrows(NotFoundException.class, () ->
-                vehicleService.calculateAvgSpeedByBrand("bmw")
+        // Act
+        NotFoundException notFoundException = assertThrows(NotFoundException.class, () ->
+                vehicleService.calculateAvgSpeedByBrand(brand)
+        );
+
+        // Assert
+        assertAll(
+                ()-> assertEquals("No se encontraron vehículos de esa marca."
+                        ,notFoundException.getMessage()),
+                ()-> assertNotNull(notFoundException)
         );
     }
 
@@ -133,17 +178,20 @@ public class VehicleServiceTest {
 
     @Test
     @DisplayName("[SUCCESS] Unit test: Get average capacity by brand")
-    void calculateAvgCapacityByBrand() {
+    void calculateAvgCapacityByBrand_ShouldReturnAvgSpeed_WhenBrandHasVehicles() {
+        String brand = "lexus";
+        double expectedAvgCapacity = 4.0D;
+
         // Arrange
         Vehicle vehicle2 = VehicleFactory.vehicle2;
         Vehicle vehicle4 = VehicleFactory.vehicle4;
         List<Vehicle> vehicleList = List.of(vehicle2, vehicle4);
-        VehicleAvgCapacityByBrandDto expected = new VehicleAvgCapacityByBrandDto(4.0D);
+        VehicleAvgCapacityByBrandDto expected = new VehicleAvgCapacityByBrandDto(expectedAvgCapacity);
 
-        when(vehicleRepository.findVehiclesByBrand("lexus")).thenReturn(vehicleList);
+        when(vehicleRepository.findVehiclesByBrand(brand)).thenReturn(vehicleList);
 
         // Act
-        VehicleAvgCapacityByBrandDto result = vehicleService.calculateAvgCapacityByBrand("lexus");
+        VehicleAvgCapacityByBrandDto result = vehicleService.calculateAvgCapacityByBrand(brand);
 
         // Assert
         assertEquals(expected, result);
@@ -151,14 +199,23 @@ public class VehicleServiceTest {
 
     @Test
     @DisplayName("[ERROR 404] Unit test: Get average capacity by brand - Not found")
-    void calculateAvgCapacityByBrandNotFound() {
+    void calculateAvgCapacityByBrand_ShouldReturnError_WhenBrandHasNoVehicles() {
+        String brand = "bmw";
+
         // Arrange
-        when(vehicleRepository.findVehiclesByBrand("bmw"))
+        when(vehicleRepository.findVehiclesByBrand(brand))
                 .thenReturn(Collections.emptyList());
 
-        // Act & Assert
-        assertThrows(NotFoundException.class, () ->
-                vehicleService.calculateAvgCapacityByBrand("bmw")
+        // Act
+        NotFoundException notFoundException = assertThrows(NotFoundException.class, () ->
+                vehicleService.calculateAvgCapacityByBrand(brand)
+        );
+
+        // Assert
+        assertAll(
+                ()-> assertEquals("No se encontraron vehículos de esa marca."
+                        ,notFoundException.getMessage()),
+                ()-> assertNotNull(notFoundException)
         );
     }
 
@@ -166,16 +223,19 @@ public class VehicleServiceTest {
 
     @Test
     @DisplayName("[SUCCESS] Unit test: Get vehicles by range of weight")
-    void searchVehiclesByRangeOfWeight() {
+    void searchVehiclesByRangeOfWeight_ShouldReturnList_WhenThereAreVehiclesInTheWeightRange() {
+        double weightMin = 168.0D;
+        double weightMax = 168.6D;
+
         // Arrange
         Vehicle vehicle2 = VehicleFactory.vehicle2;
         List<Vehicle> vehicleList = List.of(vehicle2);
         List<VehicleDto> expectedResult = List.of(VehicleFactory.convertToVehicleDto(vehicle2));
 
-        when(vehicleRepository.findVehiclesByRangeOfWeight(168.0, 168.6)).thenReturn(vehicleList);
+        when(vehicleRepository.findVehiclesByRangeOfWeight(weightMin, weightMax)).thenReturn(vehicleList);
 
         // Act
-        List<VehicleDto> result = vehicleService.searchVehiclesByRangeOfWeight(168.0, 168.6);
+        List<VehicleDto> result = vehicleService.searchVehiclesByRangeOfWeight(weightMin, weightMax);
 
         // Assert
         assertAll(
@@ -186,14 +246,24 @@ public class VehicleServiceTest {
 
     @Test
     @DisplayName("[ERROR 404] Unit test: Get vehicles by range of weight - Not found")
-    void searchVehiclesByRangeOfWeightNotFound() {
+    void searchVehiclesByRangeOfWeight_ShouldReturnError_WhenThereAreNoVehiclesInTheWeightRange() {
+        double weightMin = 200.0D;
+        double weightMax = 210.0D;
+
         // Arrange
-        when(vehicleRepository.findVehiclesByRangeOfWeight(200, 210))
+        when(vehicleRepository.findVehiclesByRangeOfWeight(weightMin, weightMax))
                 .thenReturn(Collections.emptyList());
 
-        // Act & Assert
-        assertThrows(NotFoundException.class, () ->
-                vehicleService.searchVehiclesByRangeOfWeight(200, 210)
+        // Act
+        NotFoundException notFoundException = assertThrows(NotFoundException.class, () ->
+                vehicleService.searchVehiclesByRangeOfWeight(weightMin, weightMax)
+        );
+
+        // Assert
+        assertAll(
+                ()-> assertEquals("No se encontraron vehículos en ese rango de peso."
+                        ,notFoundException.getMessage()),
+                ()-> assertNotNull(notFoundException)
         );
     }
 }
